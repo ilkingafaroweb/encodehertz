ba4b14d56c629e02a88e70387baa80a436de065e
İlkin Qafarov <113019768+ilkingafarov@users.noreply.github.com>
2024-05-30 17:13:41 +0400

    Breadcrump text sizes change

--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -10,24 +10,24 @@ interface BreadcrumbProps {
 }
 const Breadcrumb = ({ pageName, prevPageName, prevRoute }: BreadcrumbProps) => {
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+    <div className="mb-3 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       {/* <h2 className="text-title-md2 font-semibold text-black dark:text-white">
         {pageName}
       </h2> */}
       <Link 
         to={prevRoute}
-        className="inline-flex items-center justify-center gap-2.5 rounded-lg bg-gray text-black lg:hover:text-primary dark:bg-boxdark-2 dark:text-white py-2 px-4 text-center font-medium lg:px-6 xl:px-4">
+        className="inline-flex items-center justify-center gap-2.5 rounded-lg bg-gray text-black lg:text-lg lg:hover:text-primary dark:bg-boxdark-2 dark:text-white py-2 px-4 text-center font-medium lg:px-6 xl:px-4">
         <FontAwesomeIcon icon={faArrowLeft}/> Back
       </Link>
 
       <nav>
         <ol className="flex items-center gap-2">
           <li>
-            <Link className="font-medium" to={prevRoute}>
+            <Link className="font-medium lg:text-lg" to={prevRoute}>
               {prevPageName} /
             </Link>
           </li>
-          <li className="font-medium text-primary">{pageName}</li>
+          <li className="font-medium lg:text-lg text-primary">{pageName}</li>
         </ol>
       </nav>
     </div>
